807ea2b479d484419ca8966ea45cfaf93f3e3e9a
Nesac128 <nesac128@gmail.com>
2018-05-27 10:58:41 +0100

    Added setup details for automatic installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Neural Network Data Training & Classification',
+    version='0.0.1',
+    description='Python package to train neural networks to classify data',
+    author='PlanetGazer8360',
+    author_email='ml.learner.8359@gmail.com',
+    install_requires=['tensorflow==1.7.0', 'Pillow==5.1.0',
+                      'scikit-learn==0.19.1', 'numpy==1.14.3',
+                      'opencv-python==3.4.0.12', 'pandas==0.22.0']
+)
