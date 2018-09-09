a3c01a97a7a0b4fa61124301107a4439f12f803f
Nesac128 <ml.learner.8359@gmail.com>
2018-09-09 16:24:22 +0100

    Fixed import issue. Reading & writing paths fixed

--- nnir/src/pcontrol.py
+++ nnir/src/pcontrol.py
@@ -1,25 +1,25 @@
-import os
 import csv
 import pandas as pd
+import os
 
-from config import *
+from src.config import *
 
 
 class Sess:
     def add(self):
         nid = str(int(self.read())+1)
-        with open(nnir_path+'meta/ims.txt', 'w') as sessfile:
+        with open(nnir_path+'nnir/src/meta/ims.txt', 'w') as sessfile:
             sessfile.write(nid)
 
     def read(self):
-        with open(nnir_path+'meta/ims.txt', 'r') as sessfile:
+        with open(nnir_path+'nnir/src/meta/ims.txt', 'r') as sessfile:
             id = sessfile.read()
             sessfile.close()
         return str(id)
 
     def ndir(self):
-        if not os.path.exists(nnir_path+'meta/sess/'+str(self.read())):
-            os.mkdir(nnir_path+'meta/sess/'+str(self.read()))
+        if not os.path.exists(nnir_path+'nnir/src/meta/sess/'+str(self.read())):
+            os.mkdir(nnir_path+'nnir/src/meta/sess/'+str(self.read()))
 
 
 class MetaData:
@@ -27,13 +27,14 @@ class MetaData:
         pass
 
     def write(self, write_sess_id, **meta):
-        with open(nnir_path+'meta/sess/'+write_sess_id+'/meta.txt', 'a') as metadata:
+        with open(nnir_path+'nnir/src/meta/sess/'+write_sess_id+'/meta.txt', 'a') as metadata:
             for key in meta:
                 for val in meta.values():
-                    metadata.write(key.upper()+'='+val+'\n')
+                    print(val)
+                    metadata.write(str(key.upper()+'='+str(val)+'\n'))
 
     def read(self, *tags, sess_id):
-        reader = Reader(nnir_path+'meta/sess/' + str(sess_id) + '/meta.txt')
+        reader = Reader(nnir_path+'nnir/src/meta/sess/' + str(sess_id) + '/meta.txt')
         meta = reader.clean_read()
         for mt in meta:
             for tag in tags:
@@ -81,14 +82,14 @@ class PathManager:
 
         self.pfile = meta[0]
 
-        if not os.path.exists(nnir_path+'meta/sess/' + self.crt_sid + '/impaths.csv'):
-            with open(nnir_path+'meta/sess/' + self.crt_sid + '/impaths.csv', 'w') as pathfile:
+        if not os.path.exists(nnir_path+'nnir/src/meta/sess/' + self.crt_sid + '/impaths.csv'):
+            with open(nnir_path+'nnir/src/meta/sess/' + self.crt_sid + '/impaths.csv', 'w') as pathfile:
                 pathfile.close()
 
     def cpaths(self):
         reader = Reader(self.pfile)
         paths = reader.clean_read()
-        with open(nnir_path+'meta/sess/' + self.crt_sid + '/impaths.csv', 'a') as pathfile:
+        with open(nnir_path+'nnir/src/meta/sess/' + self.crt_sid + '/impaths.csv', 'a') as pathfile:
             writer = csv.writer(pathfile, delimiter='\n')
             for path in paths:
                 writer.writerow([path])
