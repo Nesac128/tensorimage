2c148e73e5a7be2d23415ae4d0687a32b681d59a
Nesac128 <ml.learner.8359@gmail.com>
2018-09-09 16:32:06 +0100

    Deleted user folder

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-# User configurations
-external_working_directory_path = '/media/planetgazer8360/NASER/External Tensorflow workspace/'
-
-# Program configurations
-nnir_path = os.getcwd()+'/'
--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class DataNotTrainableError(Exception):
-    def __init__(self, _):
-        pass
-
-
-class ColumnNumberError(Exception):
-    def __init__(self, _):
-        pass
