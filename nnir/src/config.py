a2b203e88f1ba4264fd53eacf5f17ea09186a83a
Nesac128 <ml.learner.8359@gmail.com>
2018-09-09 16:30:00 +0100

    Fixed nnir_path variable issue

---
+++ nnir/src/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+# User configurations
+external_working_directory_path = '/media/planetgazer8360/NASER/External Tensorflow workspace/'
+nnir_path = '/home/planetgazer8360/PycharmProjects/nnir/'
+
+# Program configurations
