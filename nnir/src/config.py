2785c3b85dd98f7056c222c26373cc5237b40712
Nesac128 <ml.learner.8359@gmail.com>
2018-10-05 23:02:37 +0100

    Added new required path variables

--- nnir/src/config.py
+++ nnir/src/config.py
@@ -1,8 +1,15 @@
-import os
-
-
 # User configurations
 external_working_directory_path = '/media/planetgazer8360/NASER/External Tensorflow workspace/'
 nnir_path = '/home/planetgazer8360/PycharmProjects/nnir/'
 
 # Program configurations
+training_metafile_path = nnir_path+'nnir/src/meta/training/meta.json'
+classification_metafile_path = nnir_path+'nnir/src/meta/classification/meta.json'
+sess_metafile_path = nnir_path+'nnir/src/meta/sess/meta.json'
+dataset_metafile_path = nnir_path+'nnir/src/meta/data/meta.json'
+id_management_file_path = nnir_path+'nnir/src/meta/id.json'
+accuracy_metafile_path = nnir_path+'nnir/src/meta/training/accuracy.json'
+
+base_training_data_store_path = external_working_directory_path+'data/training/'
+base_unclassified_data_store_path = external_working_directory_path+'data/unclassified/'
+
