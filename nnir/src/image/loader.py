a19dc7d68d0fe41f57b23df4db3f7ed487b4100a
Nesac128 <ml.learner.8359@gmail.com>
2018-10-05 23:12:48 +0100

    Updated to work with new JSON reading/writing metadata system. Refactored code

--- nnir/src/image/loader.py
+++ nnir/src/image/loader.py
@@ -11,68 +11,46 @@ from src.config import *
 class ImageLoader:
     def __init__(self,
                  dataset_name):
+        # Store parameter
         self.dataset_name = dataset_name
-
-        self.rgb_vals = []
-        self.imsize = []
 
         self.path_file = external_working_directory_path+'datasets/'+dataset_name+'/paths.txt'
         reader = TXTReader(self.path_file)
         reader.read_raw()
         reader.parse()
-        self.paths = reader.parsed_data
+        self.image_paths = reader.parsed_data
 
-        self.n_images = len(self.paths)
-
-        self.pixels = []
+        self.n_images = len(self.image_paths)
+        self.img_dims = []
+        self.image_data = []
 
         self.id_man = ID('dataset')
         self.id_man.read()
 
         self.MetaWriter = JSONWriter(self.id_man.id, dataset_metafile_path)
 
-    def non_mean_pixels(self):
-        dimensions = self.get_dims()
-        loading_progress = bar.Bar("Loading images: ", max=len(self.paths))
-        for pixels_n in range(len(self.paths)):
-            img = Image.open(self.paths[pixels_n])
+    def extract_image_data(self):
+        loading_progress = bar.Bar("Loading images: ", max=len(self.image_paths))
+        for pixels_n in range(len(self.image_paths)):
+            img = Image.open(self.image_paths[pixels_n])
             img_rgb = img.convert('RGB')
             im_pixels = []
-            for x in range(dimensions[pixels_n][0]):
-                for y in range(dimensions[pixels_n][1]):
+            for x in range(self.img_dims[pixels_n][0]):
+                for y in range(self.img_dims[pixels_n][1]):
+                    # print(pixels_n)
                     r, g, b = img_rgb.getpixel((x, y))
                     im_pixels.append(r)
                     im_pixels.append(g)
                     im_pixels.append(b)
             loading_progress.next()
-            self.pixels.append(im_pixels)
+            self.image_data.append(im_pixels)
 
-        return self.pixels
-
-    def get_dims(self):
-        sizes = []
+    def get_img_dims(self):
         for n in range(self.n_images):
-            img = Image.open(self.paths[n])
-            sizes.append(img.size)
-            self.imsize.append(img.size[0])
-            self.imsize.append(img.size[1])
+            img = Image.open(self.image_paths[0])
+            self.img_dims.append(img.size)
         print("Finished dimension extraction...")
-        return sizes
 
-    def main(self):
-        self.getRGB()
-        data = self.rgb_vals
-
+    def write_metadata(self):
         self.MetaWriter.update(path_file=self.path_file)
         self.MetaWriter.write()
-
-        return data, self.imsize, self.MetaWriter
-
-    def getRGB(self):
-        rgb_vals = []
-        pixels = self.non_mean_pixels()
-        reading_progress = bar.Bar("Reading images: ", max=len(self.paths))
-        for n, im_pixels in enumerate(pixels):
-            rgb_vals.append(im_pixels)
-            self.rgb_vals.append(im_pixels)
-            reading_progress.next()
--- nnir/src/image/writer.py
+++ nnir/src/image/writer.py
@@ -8,89 +8,92 @@ from src.man.writer import *
 from src.man.reader import *
 from src.meta.id import ID
 from src.config import *
+from src.man.mkdir import mkdir
 
 
 class DataWriter:
-    def __init__(self, data, filename, dataset_name, imsize, metadata_writer):
-        self.raw_data = data
+    def __init__(self, data, filename, dataset_name, img_dims, metadata_writer):
+        self.data = data
         self.filename = filename
         self.dataset_name = dataset_name
         self.metadata_writer = metadata_writer
-        self.imsize = imsize
+        self.img_dims = img_dims
 
-        if not os.path.exists(external_working_directory_path+'data/unclassified/'+self.dataset_name):
-            os.mkdir(external_working_directory_path+'data/unclassified/'+self.dataset_name)
+        self.data_dir = base_unclassified_data_store_path+self.dataset_name
+        self.csv_writer = CSVWriter(self.data_dir)
+
+        mkdir(self.data_dir)
+
+        self.n_classes = 0
 
     def main(self):
-        for image_data in self.raw_data:
-            self.writeCSV(image_data)
+        for image_data_inst in self.data:
+            self.csv_writer.write(image_data_inst)
         self.metadata_writer.update(
-            n_columns=str(len(self.raw_data[0])),
+            n_columns=str(len(self.data[0])),
             data_path=external_working_directory_path + 'data/unclassified/' + self.dataset_name + '/' + self.filename,
-            n_classes='0',
+            n_classes=self.n_classes,
             trainable='False',
-            width=self.imsize[0],
-            height=self.imsize[1]
+            width=self.img_dims[0][0],
+            height=self.img_dims[0][1]
         )
         self.metadata_writer.write()
 
-    def writeCSV(self, img):
-        with open(external_working_directory_path+'data/unclassified/'+self.dataset_name+'/' +
-                  self.filename, 'a') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(img)
-
 
 class TrainingDataWriter:
-    def __init__(self, data, filename, dataset_name, imsize, metadata_writer):
+    def __init__(self, data, filename, dataset_name, img_dims, metadata_writer):
+        """
+        :param data: data to be written to CSV file
+        :param filename: CSV filename
+        :param dataset_name: image dataset name from which to read images
+        :param imsize: size for dataset images (all sizes should be the same)
+        :param metadata_writer: metadata_writer class used in loader.py and passed to writer.py
+        """
+        # Store parameters
         self.input_data = data
         self.filename = filename
         self.dataset_name = dataset_name
-        self.imsize = imsize
-
-        self.labels_path = external_working_directory_path+'datasets/'+dataset_name+'/labels.txt'
-
+        self.img_dims = img_dims
         self.MetaWriter = metadata_writer
 
+        # Read image labels text file and store it
+        self.labels_path = external_working_directory_path+'datasets/'+dataset_name+'/labels.txt'
         self.PathReader = TXTReader(self.labels_path)
         self.PathReader.read_raw()
         self.PathReader.parse()
         self.labels = self.PathReader.parsed_data
 
+        # Read current data ID from id.json and store it
         self.id_man = ID('dataset')
         self.id_man.read()
         self.wdid = self.id_man.id
 
+        # Store path where to save output image data with labels and define CSVWriter object
         self.data_dir = external_working_directory_path+'data/training/'+self.dataset_name+'/'
+        self.csv_writer = CSVWriter(self.data_dir+self.filename)
 
-        if not os.path.exists(self.data_dir):
-            os.mkdir(self.data_dir)
+        # Create storing folder for output image data
+        mkdir(self.data_dir)
 
-    def n_classes(self):
-        return len(unique(self.labels))
+        # Store number of classes
+        self.n_classes = len(unique(self.labels))
 
-    def main(self):
-        self.write_metadata()
+    def join_data_labels(self):
         writing_progress = bar.Bar("Writing images: ", max=len(self.input_data))
         for imn in range(len(self.input_data)):
             self.input_data[imn].append(self.labels[imn])
-            self.write_csv(self.input_data[imn])
+            self.csv_writer.write(self.input_data[imn])
             writing_progress.next()
-        self.id_man.add()
-
-    def write_csv(self, img):
-        with open(external_working_directory_path+'data/training/'+self.dataset_name+'/'+self.filename, 'a') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(img)
 
     def write_metadata(self):
+        # Update necessary parameters for training process as metadata, to reduce required user input
         self.MetaWriter.update(
             n_columns=len(self.input_data[0]) + 1,
             data_path=self.data_dir + self.filename,
-            n_classes=self.n_classes(),
+            n_classes=self.n_classes,
             trainable='True',
             type='Image',
             data_len=len(self.input_data[0]),
-            width=self.imsize[0],
-            height=self.imsize[1])
+            width=self.img_dims[0][0],
+            height=self.img_dims[0][1])
         self.MetaWriter.write()
