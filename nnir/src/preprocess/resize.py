b7bacaa154c1455d0c1eb9afdf67c350f294679d
Nesac128 <ml.learner.8359@gmail.com>
2018-10-06 15:55:51 +0100

    Removed unnecessary code

--- nnir/src/preprocess/resize.py
+++ nnir/src/preprocess/resize.py
@@ -2,15 +2,11 @@ import os
 import cv2
 
 
-def resize(path, new_path, size: tuple):
-    images = os.listdir(path)
-    print(images)
+def resize(image_containing_folder_path,
+           new_folder_path,
+           size: tuple):
+    images = os.listdir(image_containing_folder_path)
     for img in images:
-        print("lol")
-        image = cv2.imread(path+'/'+img)
-        print(image.shape)
+        image = cv2.imread(image_containing_folder_path+'/'+img)
         resized_image = cv2.resize(image, size)
-        cv2.imwrite(new_path+'/'+img, resized_image)
-
-
-# resize('/media/planetgazer8360/NASER/External Tensorflow workspace/training_images/CatsAndDogs/cats/')
+        cv2.imwrite(new_folder_path+'/'+img, resized_image)
