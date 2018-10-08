0ba18639caee0f9324ba9ac4b7fb3c6a57d50b8d
Nesac128 <ml.learner.8359@gmail.com>
2018-10-08 18:38:02 +0100

    Updated variables

--- nnir/src/config.py
+++ nnir/src/config.py
@@ -3,13 +3,12 @@ external_working_directory_path = '/media/planetgazer8360/NASER/External Tensorf
 nnir_path = '/home/planetgazer8360/PycharmProjects/nnir/'
 
 # Program configurations
-training_metafile_path = nnir_path+'nnir/src/meta/training/meta.json'
-classification_metafile_path = nnir_path+'nnir/src/meta/classification/meta.json'
-sess_metafile_path = nnir_path+'nnir/src/meta/sess/meta.json'
-dataset_metafile_path = nnir_path+'nnir/src/meta/data/meta.json'
-id_management_file_path = nnir_path+'nnir/src/meta/id.json'
-accuracy_metafile_path = nnir_path+'nnir/src/meta/training/accuracy.json'
+training_metafile_path = external_working_directory_path+'metadata/training/meta.json'
+classification_metafile_path = external_working_directory_path+'metadata/classification/meta.json'
+dataset_metafile_path = external_working_directory_path+'metadata/data/meta.json'
+id_management_file_path = external_working_directory_path+'metadata/id.json'
+accuracy_metafile_path = external_working_directory_path+'metadata/training/accuracy.json'
+nid_names_metafile_path = external_working_directory_path+'metadata/nid_names.json'
 
-base_training_data_store_path = external_working_directory_path+'data/training/'
-base_unclassified_data_store_path = external_working_directory_path+'data/unclassified/'
-
+base_training_data_store_path = external_working_directory_path+'user/data/training/'
+base_unclassified_data_store_path = external_working_directory_path+'user/data/unclassified/'
