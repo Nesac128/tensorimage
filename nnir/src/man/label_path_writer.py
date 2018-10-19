b3701d5977ca0da3236a52fd42cc1bd945a3a57a
Nesac128 <ml.learner.8359@gmail.com>
2018-10-19 22:24:48 +0100

    Updated & fixed code

--- nnir/src/man/label_path_writer.py
+++ nnir/src/man/label_path_writer.py
@@ -8,7 +8,7 @@ from src.config import external_working_directory_path
 
 
 def write_labels(main_directory_path, dataset_name, label_file_name='labels.txt'):
-    bspath = external_working_directory_path + main_directory_path + '/'
+    bspath = external_working_directory_path + 'user/' + main_directory_path + '/'
 
     folders = os.listdir(bspath)
     n_images = 0
@@ -22,10 +22,10 @@ def write_labels(main_directory_path, dataset_name, label_file_name='labels.txt'
 
     obj_to_lb = {}
 
-    if not os.path.exists(external_working_directory_path + 'datasets/' + dataset_name):
-        os.mkdir(external_working_directory_path + 'datasets/' + dataset_name)
+    if not os.path.exists(external_working_directory_path + 'user/datasets/' + dataset_name):
+        os.mkdir(external_working_directory_path + 'user/datasets/' + dataset_name)
 
-    with open(external_working_directory_path + 'datasets/' + dataset_name + '/' + label_file_name, 'a') as labels:
+    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/' + label_file_name, 'a') as labels:
         writing_progress = bar.Bar("Writing image labels: ", max=n_images)
         for i, folder in enumerate(folders):
             for ftp in os.listdir(bspath+'/'+folder):
@@ -33,23 +33,23 @@ def write_labels(main_directory_path, dataset_name, label_file_name='labels.txt'
                 writing_progress.next()
             obj_to_lb[folder.split('-')[0]] = keys[i]
 
-    with open(external_working_directory_path + 'datasets/' + dataset_name + '/obj_labels.json', 'w') as txt:
+    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/obj_labels.json', 'w') as txt:
         json.dump(obj_to_lb, txt, indent=3, sort_keys=True)
     print("")
 
 
 def write_paths(main_directory_path, dataset_name):
-    bspath = external_working_directory_path+main_directory_path+'/'
+    bspath = external_working_directory_path+'user/'+main_directory_path+'/'
 
     folders = os.listdir(bspath)
     n_images = 0
     for folder in folders:
         n_images += len(os.listdir(bspath+folder))
 
-    if not os.path.exists(external_working_directory_path + 'datasets/' + dataset_name):
-        os.mkdir(external_working_directory_path + 'datasets/' + dataset_name)
+    if not os.path.exists(external_working_directory_path + 'user/datasets/' + dataset_name):
+        os.mkdir(external_working_directory_path + 'user/datasets/' + dataset_name)
 
-    with open(external_working_directory_path + 'datasets/' + dataset_name + '/paths.txt', 'a') as paths:
+    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/paths.txt', 'a') as paths:
         writing_progress = bar.Bar("Writing image paths: ", max=n_images)
         for n, folder in enumerate(folders):
             images = os.listdir(bspath+folder)
