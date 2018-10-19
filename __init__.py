0117c8b147dc5b497257fc581c08bc2378fefc23
Nesac128 <ml.learner.8359@gmail.com>
2018-10-19 22:23:06 +0100

    Made refractoring and updated __version__

--- __init__.py
+++ __init__.py
@@ -1,17 +1,29 @@
-import os
 from nnir.src.config import external_working_directory_path
+from nnir.src.man.mkdir import mkdir
+from nnir.src.man.mkemptfile import mkemptfile
+from nnir.src.man.writer import JSONWriter
 
-if not os.path.exists(external_working_directory_path):
-    os.mkdir(external_working_directory_path)
-if not os.path.exists(external_working_directory_path+'/datasets'):
-    os.mkdir(external_working_directory_path+'/datasets')
-if not os.path.exists(external_working_directory_path+'/trained_models'):
-    os.mkdir(external_working_directory_path+'/trained_models')
-if not os.path.exists(external_working_directory_path+'/data'):
-    os.mkdir(external_working_directory_path+'/data')
-if not os.path.exists(external_working_directory_path+'/training_images'):
-    os.mkdir(external_working_directory_path+'/training_images')
-if not os.path.exists(external_working_directory_path+'testing_images'):
-    os.mkdir(external_working_directory_path+'/testing_images')
+dir_paths = [external_working_directory_path,
+             external_working_directory_path + 'user',
+             external_working_directory_path + 'user/datasets',
+             external_working_directory_path + 'user/trained_models',
+             external_working_directory_path + 'user/data',
+             external_working_directory_path + 'user/training_images',
+             external_working_directory_path + 'user/unclassified_images',
+             external_working_directory_path + 'user/layer_activations',
+             external_working_directory_path + 'metadata',
+             external_working_directory_path + 'metadata/classification',
+             external_working_directory_path + 'metadata/data',
+             external_working_directory_path + 'metadata/training']
 
-__version__ = 'v1.0.0-beta'
+file_paths = [external_working_directory_path + 'metadata/id.json',
+              external_working_directory_path + 'metadata/classification/meta.json',
+              external_working_directory_path + 'metadata/data/meta.json',
+              external_working_directory_path + 'metadata/training/meta.json',
+              external_working_directory_path + 'metadata/training/accuracy.json']
+
+
+for dir_path in dir_paths:
+    mkdir(dir_path)
+
+__version__ = 'v1.0.0'
