b6a53cf2e4b346911aceecac0aaf57c3a15beaac
Nesac128 <ml.learner.8359@gmail.com>
2018-10-21 11:52:21 +0100

    Updated to remove program generated labels, to use class names directly. Also made consequently necessary changes

--- nnir/src/man/label_path_writer.py
+++ nnir/src/man/label_path_writer.py
@@ -7,7 +7,7 @@ from progress import bar
 from src.config import external_working_directory_path
 
 
-def write_labels(main_directory_path, dataset_name, label_file_name='labels.txt'):
+def write_labels(main_directory_path, dataset_name):
     bspath = external_working_directory_path + 'user/' + main_directory_path + '/'
 
     folders = os.listdir(bspath)
@@ -15,27 +15,21 @@ def write_labels(main_directory_path, dataset_name, label_file_name='labels.txt'
     for folder in folders:
         n_images += len(os.listdir(bspath + folder))
 
-    keys = []
-
-    for k in product(string.ascii_uppercase, repeat=2):
-        keys.append(k[0] + k[1])
-
-    obj_to_lb = {}
+    class_id = {}
 
     if not os.path.exists(external_working_directory_path + 'user/datasets/' + dataset_name):
         os.mkdir(external_working_directory_path + 'user/datasets/' + dataset_name)
 
-    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/' + label_file_name, 'a') as labels:
+    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/labels.txt', 'a') as labels:
         writing_progress = bar.Bar("Writing image labels: ", max=n_images)
-        for i, folder in enumerate(folders):
-            for ftp in os.listdir(bspath+'/'+folder):
-                labels.write(keys[i] + '\n')
+        for i, folder_name in enumerate(folders):
+            for image in os.listdir(bspath+'/'+folder_name):
+                labels.write(folder_name + '\n')
                 writing_progress.next()
-            obj_to_lb[folder.split('-')[0]] = keys[i]
+            class_id[i] = folder_name
 
-    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/obj_labels.json', 'w') as txt:
-        json.dump(obj_to_lb, txt, indent=3, sort_keys=True)
-    print("")
+    with open(external_working_directory_path + 'user/datasets/' + dataset_name + '/class_id.json', 'w') as txt:
+        json.dump(class_id, txt, indent=3, sort_keys=True)
 
 
 def write_paths(main_directory_path, dataset_name):
