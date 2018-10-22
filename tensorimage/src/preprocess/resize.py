13f791795b845709dc06a808bc6c8754e37ed35a
Nesac128 <ml.learner.8359@gmail.com>
2018-10-22 17:00:50 +0100

    Added new function & improved code

--- tensorimage/src/preprocess/resize.py
+++ tensorimage/src/preprocess/resize.py
@@ -1,12 +1,29 @@
 import os
 import cv2
+from progress.bar import Bar
+
+from src.config import workspace_dir
 
 
-def resize(image_containing_folder_path,
-           new_folder_path,
-           size: tuple):
-    images = os.listdir(image_containing_folder_path)
-    for img in images:
-        image = cv2.imread(image_containing_folder_path+'/'+img)
-        resized_image = cv2.resize(image, size)
-        cv2.imwrite(new_folder_path+'/'+img, resized_image)
+def resize_training_dataset(dataset_name,
+                            size: tuple):
+    folders = os.listdir(dataset_name)
+    resize_progress = Bar('Resizing images: ', max=len(folders))
+    for folder in folders:
+        images = os.listdir(workspace_dir+'users/training_images/'+dataset_name+'/'+folder)
+        for image in images:
+            image_ = cv2.imread(workspace_dir+'user/training_images/'+dataset_name+'/'+folder+'/'+image)
+            resized_image = cv2.resize(image_, size)
+            cv2.imwrite(workspace_dir+'user/training_images/'+dataset_name+'/'+folder+'/'+image, resized_image)
+        resize_progress.next()
+
+
+def resize_unclassified_dataset(dataset_name,
+                                size: tuple):
+    images = os.listdir(dataset_name)
+    resize_progress = Bar('Resizing images: ', max=len(images))
+    for image in images:
+        image_ = cv2.imread(workspace_dir+'user/unclassified_images/'+dataset_name+'/'+image)
+        resized_image = cv2.resize(image_, size)
+        cv2.imwrite(workspace_dir+'user/unclassified_images/'+dataset_name+'/'+image, resized_image)
+        resize_progress.next()
