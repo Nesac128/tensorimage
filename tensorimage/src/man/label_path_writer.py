c26ad5482b782ef90308b2dfeebf4f1b73c26ad4
Nesac128 <ml.learner.8359@gmail.com>
2018-10-26 23:26:28 +0100

    Updated code to work for new workspace structuring

--- tensorimage/src/man/label_path_writer.py
+++ tensorimage/src/man/label_path_writer.py
@@ -6,61 +6,55 @@ from src.config import workspace_dir
 from src.man.mkdir import mkdir
 
 
-def write_labels(dataset_name):
-    bspath = workspace_dir + 'user/training_images/' + dataset_name + '/'
-    dataset_dir = workspace_dir + 'user/datasets/' + dataset_name
+def write_unclassified_dataset_paths(dataset_path, dataset_name):
+    dataset_dir = workspace_dir+'user/unclassified_datasets/'+dataset_name
     mkdir(dataset_dir)
 
-    folders = os.listdir(bspath)
+    images = os.listdir(dataset_path)
+    n_images = len(images)
+
+    writing_progress = bar.Bar("Writing image paths: ", max=n_images)
+    with open(dataset_dir+'/paths.txt', 'a') as paths:
+        for image in images:
+            paths.write(dataset_path+'/'+image+'\n')
+            writing_progress.next()
+
+
+def write_training_dataset_paths(dataset_path, dataset_name):
+    dataset_dir = workspace_dir + 'user/training_datasets/' + dataset_name
+    mkdir(dataset_dir)
+    folders = os.listdir(dataset_path)
     n_images = 0
     for folder in folders:
-        n_images += len(os.listdir(bspath + folder))
+        n_images += len(os.listdir(dataset_path+'/'+folder))
+
+    with open(dataset_dir + '/paths.txt', 'a') as paths:
+        writing_progress = bar.Bar("Writing image paths: ", max=n_images)
+        for n, folder in enumerate(folders):
+            images = os.listdir(dataset_path+'/'+folder)
+            for tp in images:
+                paths.write(dataset_path+'/'+folder+'/'+tp+'\n')
+                writing_progress.next()
+
+
+def write_labels(dataset_path, dataset_name):
+    dataset_dir = workspace_dir + 'user/training_datasets/' + dataset_name
+    mkdir(dataset_dir)
+
+    folders = os.listdir(dataset_path)
+    n_images = 0
+    for folder in folders:
+        n_images += len(os.listdir(dataset_path+'/'+folder))
 
     class_id = {}
 
     with open(dataset_dir + '/labels.txt', 'a') as labels:
         writing_progress = bar.Bar("Writing image labels: ", max=n_images)
         for i, folder_name in enumerate(folders):
-            for image in os.listdir(bspath+'/'+folder_name):
+            for image in os.listdir(dataset_path+'/'+folder_name):
                 labels.write(folder_name + '\n')
                 writing_progress.next()
             class_id[i] = folder_name
 
-    with open(workspace_dir + 'user/datasets/' + dataset_name + '/class_id.json', 'w') as txt:
+    with open(dataset_dir + '/class_id.json', 'w') as txt:
         json.dump(class_id, txt, indent=3, sort_keys=True)
-
-
-def write_training_dataset_paths(dataset_name):
-    bspath = workspace_dir+'user/training_images/'+dataset_name+'/'
-    dataset_dir = workspace_dir + 'user/datasets/' + dataset_name
-    mkdir(dataset_dir)
-
-    folders = os.listdir(bspath)
-    n_images = 0
-    for folder in folders:
-        n_images += len(os.listdir(bspath+folder))
-
-    with open(dataset_dir + '/paths.txt', 'a') as paths:
-        writing_progress = bar.Bar("Writing image paths: ", max=n_images)
-        for n, folder in enumerate(folders):
-            images = os.listdir(bspath+folder)
-            for tp in images:
-                paths.write(bspath+folder+'/'+tp+'\n')
-                writing_progress.next()
-
-
-def write_unclassified_dataset_paths(dataset_name):
-    bspath = workspace_dir+'user/unclassified_images/'+dataset_name+'/'
-    dataset_dir = workspace_dir+'user/datasets/'+dataset_name
-    mkdir(dataset_dir)
-
-    images = os.listdir(bspath)
-    n_images = len(images)
-
-    writing_progress = bar.Bar("Writing image paths: ", max=n_images)
-    with open(dataset_dir+'/paths.txt', 'a') as paths:
-        for image in images:
-            paths.write(bspath+image)
-            writing_progress.next()
-
-
