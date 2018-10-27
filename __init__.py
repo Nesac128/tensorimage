ce8ec3627cc7104617eb5fce76fd15345a75dbaf
Nesac128 <ml.learner.8359@gmail.com>
2018-10-27 22:41:08 +0100

    Modified for new workspace structure

--- __init__.py
+++ __init__.py
@@ -1,31 +1,30 @@
 from tensorimage.src.config import workspace_dir
 from tensorimage.src.man.mkdir import mkdir
-from tensorimage.src.man.mkemptfile import mkemptfile
+from tensorimage.src.man.make_file import make_json_file
 
 dir_paths = [workspace_dir,
              workspace_dir + 'user',
-             workspace_dir + 'user/datasets',
              workspace_dir + 'user/trained_models',
-             workspace_dir + 'user/data',
-             workspace_dir + 'user/training_images',
-             workspace_dir + 'user/unclassified_images',
-             workspace_dir + 'user/layer_activations',
+             workspace_dir + 'user/training_datasets',
+             workspace_dir + 'user/unclassified_datasets',
+             workspace_dir + 'user/logs',
+             workspace_dir + 'user/predictions',
              workspace_dir + 'metadata',
-             workspace_dir + 'metadata/classification',
              workspace_dir + 'metadata/data',
              workspace_dir + 'metadata/training']
 
 file_paths = [workspace_dir + 'metadata/id.json',
-              workspace_dir + 'metadata/classification/meta.json',
               workspace_dir + 'metadata/data/meta.json',
-              workspace_dir + 'metadata/training/meta.json',
-              workspace_dir + 'metadata/training/accuracy.json']
+              workspace_dir + 'metadata/training/meta.json']
 
 
 for dir_path in dir_paths:
     mkdir(dir_path)
 
 for file_path in file_paths:
-    mkemptfile(file_path)
+    make_json_file(file_path, {
+        "1": {"id": 0},
+        "2": {"id": 0},
+        "3": {"id": 0}}) if 'id.' in file_path else make_json_file(file_path, {})
 
-__version__ = 'v1.0.0'
+__version__ = 'v1.1.0'
