da7f972339e74da7cbb99ce108a532ce13832e22
Nesac128 <ml.learner.8359@gmail.com>
2018-10-29 15:32:04 +0000

    Added lighting changing option

--- tensorimage/tools/train/augment_data.py
+++ tensorimage/tools/train/augment_data.py
@@ -14,22 +14,25 @@ class AugmentData:
 
         self.n_images = self.x.shape[0]
 
+    def _copy_xy(self):
+        return np.copy(self.x), np.copy(self.y)
+
     def flip_images(self):
         augmented_data = tf.constant([], tf.float32, shape=[0, self.dims[0], self.dims[1], self.n_channels])
         augmented_labels = tf.constant([], tf.float32, shape=[0, self.n_classes])
-        for n in range(self.n_images):
-            flip_1 = tf.image.flip_up_down(self.x[n])
-            flip_2 = tf.image.flip_left_right(self.x[n])
-            flip_3 = tf.image.random_flip_up_down(self.x[n])
-            flip_4 = tf.image.random_flip_left_right(self.x[n])
+        for image, label in zip(self.x, self.y):
+            flip_up_down = tf.image.flip_up_down(image)
+            flip_left_right = tf.image.flip_left_right(image)
+            random_flip_up_down = tf.image.random_flip_up_down(image)
+            random_flip_left_right = tf.image.random_flip_left_right(image)
 
-            data = tf.concat([tf.expand_dims(self.x[n], 0), tf.expand_dims(flip_1, 0),
-                              tf.expand_dims(flip_2, 0), tf.expand_dims(flip_3, 0),
-                              tf.expand_dims(flip_4, 0)], 0)
+            data = tf.concat([tf.expand_dims(image, 0), tf.expand_dims(flip_up_down, 0),
+                              tf.expand_dims(flip_left_right, 0), tf.expand_dims(random_flip_up_down, 0),
+                              tf.expand_dims(random_flip_left_right, 0)], 0)
 
-            labels = tf.concat([tf.expand_dims(self.y[n], 0), tf.expand_dims(self.y[n], 0),
-                                tf.expand_dims(self.y[n], 0), tf.expand_dims(self.y[n], 0),
-                                tf.expand_dims(self.y[n], 0)], 0)
+            labels = tf.concat([tf.expand_dims(label, 0), tf.expand_dims(label, 0),
+                                tf.expand_dims(label, 0), tf.expand_dims(label, 0),
+                                tf.expand_dims(label, 0)], 0)
             augmented_data = tf.concat([augmented_data, tf.cast(data, tf.float32)], 0)
             augmented_labels = tf.concat([augmented_labels, tf.cast(labels, tf.float32)], 0)
         return augmented_data, augmented_labels
@@ -38,13 +41,25 @@ class AugmentData:
         salt_n = np.ceil(amount * self.x[0].size * salt_vs_pepper)
         pepper_n = np.ceil(amount * self.x[0].size * (1.0 - salt_vs_pepper))
 
-        images_copy = np.copy(self.x)
+        images_copy, labels_copy = self._copy_xy()
 
-        for n, image in enumerate(images_copy):
+        for img_n, image, lb_n, label in zip(enumerate(images_copy), enumerate(labels_copy)):
             salt = [np.random.randint(0, i - 1, int(salt_n)) for i in image.shape]
-            images_copy[n][salt[0], salt[1], :] = 1
+            images_copy[img_n][salt[0], salt[1], :] = 1
 
             coords = [np.random.randint(0, i - 1, int(pepper_n)) for i in image.shape]
-            images_copy[n][coords[0], coords[1], :] = 0
-        return np.concatenate((self.x, images_copy))
+            images_copy[img_n][coords[0], coords[1], :] = 0
 
+            labels_copy[lb_n] = label
+        return np.concatenate((self.x, images_copy)), np.concatenate((self.y, labels_copy))
+
+    def modify_lighting(self, max_delta: float):
+        images_copy, labels_copy = self._copy_xy()
+
+        for img_n, image, lb_n, label in zip(enumerate(self.x), enumerate(self.y)):
+            random_brightness_image = tf.image.random_brightness(image, max_delta)
+            images_copy[img_n] = random_brightness_image
+            labels_copy[lb_n] = label
+        return np.concatenate((self.x, images_copy)), np.concatenate((self.y, labels_copy))
+
+    
