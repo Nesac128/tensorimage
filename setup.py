b3ad38e05672b7dd16e973e715543ee864784cef
Nesac128 <ml.learner.8359@gmail.com>
2018-11-22 21:20:01 +0000

    Updated setup.py and created requirements.txt

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tensorimage',
-    version='1.1.0',
+    version='1.2.0',
     description='Machine learning image classification tool',
     long_description=long_description,
     author='TensorImage',
     author_email='tensor.image2@gmail.com',
     long_description_content_type="text/markdown",
     url="https://github.com/TensorImage/TensorImage",
-    install_requires=['tensorflow==1.9.0', 'tensorboard', 'Pillow==5.1.0',
-                      'scikit-learn==0.19.1', 'numpy==1.14.5', 'pandas==0.22.0',
-                      'opencv-python==3.4.0.12', 'click==6.7', 'progress'],
+    install_requires=['tensorflow>=1.9.0', 'tensorboard', 'Pillow>=5.1.0',
+                      'scikit-learn>=0.19.1', 'numpy>=1.14.5', 'pandas>=0.22.0',
+                      'opencv-python>=3.4.0.12', 'progress'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL-2.0 License",
