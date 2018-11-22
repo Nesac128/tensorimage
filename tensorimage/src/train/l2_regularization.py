3285cd496904fa9b77ad65d145ced72b5915ee45
Nesac128 <ml.learner.8359@gmail.com>
2018-11-22 16:41:37 +0000

    Made usage simpler

--- tensorimage/src/train/l2_regularization.py
+++ tensorimage/src/train/l2_regularization.py
@@ -1,15 +1,14 @@
 import tensorflow as tf
-from tensorimage.ops.models import *
 from tensorimage.src.weights_initializer import init_weights
 
 
 class L2RegularizationBuilder:
-    def __init__(self, architecture: classmethod, l2_reg_beta):
+    def __init__(self, architecture: str, l2_reg_beta: float):
         self.architecture = architecture
         self.l2_reg_beta = l2_reg_beta
 
     def start(self):
-        if self.architecture.__doc__ == RosNetModel.__doc__:
+        if self.architecture == 'RosNet':
             return (self.l2_reg_beta * tf.nn.l2_loss(init_weights('weights', 'conv1', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('biases', 'conv1', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('weights', 'conv2', None)) +
@@ -18,7 +17,7 @@ class L2RegularizationBuilder:
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('biases', 'fcl', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('weights', 'out', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('biases', 'out', None)))
-        elif self.architecture.__doc__ == AlexNetModel.__doc__:
+        elif self.architecture == 'AlexNet':
             return (self.l2_reg_beta * tf.nn.l2_loss(init_weights('weights', 'conv1', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('biases', 'conv1', None)) +
                     self.l2_reg_beta * tf.nn.l2_loss(init_weights('weights', 'conv2', None)) +
