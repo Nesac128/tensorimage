eb4cf035d532b061c69bf499451b416441be881e
Nesac128 <ml.learner.8359@gmail.com>
2018-11-29 17:00:44 +0000

    Added feature and refactored

--- tensorimage/src/convnet_builder.py
+++ tensorimage/src/convnet_builder.py
@@ -1,12 +1,26 @@
 from tensorimage.src.train import models
 
 
+architectures = [
+    ('AlexNet', models.AlexNet),
+    ('RosNet', models.RosNet),
+]
+
+
 class ConvNetBuilder:
     def __init__(self, architecture):
         self.architecture = architecture
 
     def build_convnet(self):
-        if self.architecture == 'AlexNet':
-            return models.AlexNet
-        elif self.architecture == 'RosNet':
-            return models.RosNet
+        for avarchs in architectures:
+            if self.architecture == avarchs[0]:
+                return avarchs[1]
+
+
+def check_exist(architecture):
+    for avarch in architectures:
+        if architecture == avarch[0]:
+            return True
+        else:
+            continue
+    return False
