4f8e68399d7eae4fb1381b4e000d8fd09dd85d84
Nesac128 <ml.learner.8359@gmail.com>
2018-12-08 14:08:17 +0000

    Updated version and added new requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='TensorImage',
-    version='1.2.0',
+    version='1.2.6',
     description='Machine learning image classification library',
     long_description=long_description,
     author='TensorImage',
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/TensorImage/TensorImage",
     install_requires=['tensorflow>=1.9.0', 'tensorboard', 'Pillow>=5.1.0',
                       'scikit-learn>=0.19.1', 'numpy>=1.14.5', 'pandas>=0.22.0',
-                      'opencv-python>=3.4.0.12', 'progress'],
+                      'opencv-python>=3.4.0.12', 'progress', 'scipy'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX")
