b56bf6c8bacd82245e9ac03ea84ed12cbb65b609
Nesac128 <ml.learner.8359@gmail.com>
2019-01-19 22:24:07 +0000

    Added 4 new operations

--- tensorimage/data_augmentation/ops.py
+++ tensorimage/data_augmentation/ops.py
@@ -9,8 +9,8 @@ class AddPepperSaltNoise:
         self.amount = amount
 
 
-class ModifyLighting:
-    """modify_lighting"""
+class RandomBrightness:
+    """random_brightness"""
     def __init__(self, max_delta: float):
         self.max_delta = max_delta
 
@@ -18,4 +18,31 @@ class ModifyLighting:
 class GaussianBlur:
     """gaussian_blur"""
     def __init__(self, sigma=1):
-        self.sigma=sigma
+        self.sigma = sigma
+
+
+class ColorFilter:
+    """color_filter"""
+    def __init__(self, lower_hsv_bound: tuple, upper_hsv_bound: tuple):
+        self.lower_hsv_bound = lower_hsv_bound
+        self.upper_hsv_bound = upper_hsv_bound
+
+
+class RandomHue:
+    """random_hue"""
+    def __init__(self, max_delta):
+        self.max_delta = max_delta
+
+
+class RandomContrast:
+    """random_contrast"""
+    def __init__(self, lower_contrast_bound, upper_contrast_bound):
+        self.lower_contrast_bound = lower_contrast_bound
+        self.upper_contrast_bound = upper_contrast_bound
+
+
+class RandomSaturation:
+    """random_saturation"""
+    def __init__(self, lower_saturation_bound, upper_saturation_bound):
+        self.lower_saturation_bound = lower_saturation_bound
+        self.upper_saturation_bound = upper_saturation_bound
