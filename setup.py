8472e8eb594db2738f1f88ccf6a6ea7ab8401dcd
Nesac128 <ml.learner.8359@gmail.com>
2019-02-16 21:38:53 +0000

    Updated version to v2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='TensorImage',
-    version='2.0.2',
+    version='2.1.0',
     description='Image classification library for easily and quickly deploying models and training classifiers',
     long_description=long_description,
     author='TensorImage',
